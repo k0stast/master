6b704db487d368d5519e43d708634f7fd1e426c8
Kostas <138611013+k0stast@users.noreply.github.com>
2024-12-20 18:34:06 +0000

    init commit

---
+++ src/main/java/com/example/master/Controller.java
@@ -0,0 +1,13 @@
+package com.example.master;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
